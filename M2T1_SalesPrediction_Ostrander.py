860e0d3fb462c02aff82f06f3b7a0041b66b33d6
ostrandr6399 <ostrandr6399@student.faytechcc.edu>
2017-06-23 10:09:03 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Ostrander.py
@@ -0,0 +1,8 @@
+# A brief description of the project
+# Date
+# CTI-110 M2T1 - Sales Prediction
+# Richard K. Ostrander
+
+total_sales = float(input('Enter estimated sales: '))
+profit = total_sales * 0.23
+print ('Your Profit is $', profit)
